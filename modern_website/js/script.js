a3792513fad4acc268cd5fb604c2fa62e5cd0cac
Felipe Vilella <vilella.luisfelipe@gmail.com>
2020-03-31 18:29:10 -0300

    put animation phrase

--- modern_website/js/script.js
+++ modern_website/js/script.js
@@ -1,7 +1,15 @@
 jQuery(document).ready(function(){
     "use strict"
     $('.slider').ripples({
-        dropRadius: 13,
+        dropRadius: 11,
         perturbance: 0.01,
     });
+
+    var typed = new Typed('#text-txt', {
+        strings:["I just <span class='primary'>learning...</span>", "and it's <span class='second'>awesome!!!</span>"],
+        typeSpeed: 35,
+        backSpeed: 20,
+        loop: true,
+    });
+
 });
