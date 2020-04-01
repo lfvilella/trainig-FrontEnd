2316855e30b0b934f847085492bb6d26da913c75
Felipe Vilella <vilella.luisfelipe@gmail.com>
2020-04-01 10:51:30 -0300

    building(services part)

--- modern_website/js/script.js
+++ modern_website/js/script.js
@@ -6,10 +6,21 @@ jQuery(document).ready(function(){
     });
 
     var typed = new Typed('#text-txt', {
-        strings:["I just <span class='primary'>learning...</span>", "and it's <span class='second'>awesome!!!</span>"],
+        strings:["I'm just <span class='primary'>learning...</span>", "and it's <span class='second'>awesome!!!</span>"],
         typeSpeed: 35,
         backSpeed: 20,
         loop: true,
     });
 
-});
+    $(window).scroll(function(){
+        var top = $(window).scrollTop();
+        if (top>=60){
+            $("nav").addClass('secondary-bg')
+        }
+        else{
+            if($("nav").hasClass('secondary-bg')){
+                $("nav").removeClass('secondary-bg');
+            }
+        }
+    });
+});r
